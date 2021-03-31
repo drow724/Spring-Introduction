696c24629ad609769e68a2b70d0ec8190d355343
JAEGEUN SONG <76800974+drow724@users.noreply.github.com>
2021-03-31 20:56:02 +0900

    AOP 적용
    회원가입, 회원 조회등 핵심 관심사항과 시간을 측정하는 공통 관심 사항을 분리한다.
    시간을 측정하는 로직을 별도의 공통 로직으로 만들었다.
    핵심 관심 사항을 깔끔하게 유지할 수 있다.
    변경이 필요하면 이 로직만 변경하면 된다.
    원하는 적용 대상을 선택할 수 있다.

--- src/main/java/hello/hellospring/SpringConfig.java
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -1,5 +1,6 @@
 package hello.hellospring;
 
+import hello.hellospring.aop.TimeTraceAop;
 import hello.hellospring.repository.*;
 import hello.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,6 +23,10 @@ public class SpringConfig {
         return new MemberService(memberRepository);
     }
 
+//    @Bean
+//    public TimeTraceAop timeTraceAop() {
+//        return new TimeTraceAop();
+//    }
 //    @Bean
 //    public MemberRepository memberRepository() {
 //        return new MemoryMemberRepository();
--- src/main/java/hello/hellospring/service/MemberService.java
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -26,18 +26,10 @@ public class MemberService {
 
 
     public Long join(Member member) {
-        //같은 이름이 있는 중복 회원X
 
-        Long start = System.currentTimeMillis();
-        try {
             validateDuplicateMember(member);//중복 회원 검증
             memberRepository.save(member);
             return member.getId();
-        } finally {
-            long finish = System.currentTimeMillis();
-            long timeMs = finish - start;
-            System.out.println("join = " + timeMs + "ms");
-        }
 
     }
     private void validateDuplicateMember(Member member) {
@@ -51,14 +43,8 @@ public class MemberService {
      * 전체 회원 조회
      */
     public List<Member> findMembers(){
-        Long start = System.currentTimeMillis();
-        try {
             return memberRepository.findAll();
-        } finally {
-            long finish = System.currentTimeMillis();
-            long timeMs = finish - start;
-            System.out.println("findMembers " + timeMs + "ms");
-        }
+
     }
 
     public Optional<Member> findOne(Long memberId){
