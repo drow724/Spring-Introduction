f7e6dee3d8b3e611ea0003b90b14076031adef9c
JAEGEUN SONG <76800974+drow724@users.noreply.github.com>
2021-03-31 20:36:20 +0900

    AOP가 필요한 상황
    모든 메소드의 호출 시간을 측정하고 싶다면?
    회원 가입 시간, 회원 조회 시간을 측정하고 싶다면?

--- src/main/java/hello/hellospring/service/MemberService.java
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -27,11 +27,19 @@ public class MemberService {
 
     public Long join(Member member) {
         //같은 이름이 있는 중복 회원X
-        validateDuplicateMember(member);//중복 회원 검증
-        memberRepository.save(member);
-        return member.getId();
-    }
 
+        Long start = System.currentTimeMillis();
+        try {
+            validateDuplicateMember(member);//중복 회원 검증
+            memberRepository.save(member);
+            return member.getId();
+        } finally {
+            long finish = System.currentTimeMillis();
+            long timeMs = finish - start;
+            System.out.println("join = " + timeMs + "ms");
+        }
+
+    }
     private void validateDuplicateMember(Member member) {
         memberRepository.findByName(member.getName())
                 .ifPresent(m -> {
@@ -43,7 +51,14 @@ public class MemberService {
      * 전체 회원 조회
      */
     public List<Member> findMembers(){
-        return memberRepository.findAll();
+        Long start = System.currentTimeMillis();
+        try {
+            return memberRepository.findAll();
+        } finally {
+            long finish = System.currentTimeMillis();
+            long timeMs = finish - start;
+            System.out.println("findMembers " + timeMs + "ms");
+        }
     }
 
     public Optional<Member> findOne(Long memberId){
